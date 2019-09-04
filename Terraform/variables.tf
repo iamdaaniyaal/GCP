ee8f99f76a2cce3943908e6298343ff70ed16ab5
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-09-04 19:46:49 +0530

    Added base infra
    Added VPC, Static IP, Forwarding Rules, VPN-Gateway & Tunnel, Storage Bucket-LCR & ACL, IAM, Service Account, Enabling API

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -5,6 +5,8 @@ variable "vpc1_name" {}
 variable "vpc2_name" {}
 variable "subnet1_cidr" {}
 variable "subnet2_cidr" {}
+variable "subnet1_source_ranges" {}
+variable "subnet2_source_ranges" {}
 variable "firewall_protocol1" {}
 variable "firewall_protocol2" {}
 variable "firewall_ports" {
@@ -13,3 +15,47 @@ variable "firewall_ports" {
 
 variable "vpn_gateway1" {}
 variable "vpn_gateway2" {}
+
+variable "ip_address_name1" {}
+variable "ip_address_name2" {}
+
+
+
+variable "fr_esp" {}
+variable "fr_esp_ip_protocol" {}
+
+
+variable "fr_udp500" {}
+variable "fr_udp500_ip_protocol" {}
+variable "fr_udp500_port_range" {}
+
+
+variable "fr_udp4500" {}
+variable "fr_udp4500_ip_protocol" {}
+variable "fr_udp4500_port_range" {}
+ 
+
+
+variable "vpn_tunnel1" {}
+variable "vpn_tunnel2" {}
+
+variable "local_traffic_selector" {}
+
+
+variable "iam_role" {}
+variable "iam_member" {}
+variable "storage_bucket_name" {}
+variable "storage_bucket_location" {}
+variable "storage_bucket_class" {}
+variable "storage_bucket_lcr_action_type" {}
+variable "storage_bucket_lcr_condition_age" {}
+variable "storage_bucket_versioning" {}
+variable "storage_bucket_acl_role" { 
+		type = list(string)
+}
+
+
+
+variable "storage_json_api" {}
+variable "cloud_storage_api" {}
+
