8234673275cee5f309095e9d6be6a1a303e93a0d
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-09-10 12:11:52 +0530

    Update script.sh

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -4,3 +4,6 @@ sudo unzip latest.zip
 sudo apt-get install apache2 -y
 sudo systemctl enable apache2
 sudo systemctl start apache2
+sudo rsync -av wordpress/* /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
