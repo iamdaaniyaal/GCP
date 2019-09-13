e609cd91b57201f155ea4cb044daee54e80ead3d
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-09-13 11:05:28 +0530

    Update script.sh

--- Terraform/script.sh
+++ Terraform/script.sh
@@ -1,9 +1,13 @@
-sudo wget https://wordpress.org/latest.zip
-sudo apt-get install unzip -y
-sudo unzip latest.zip
+sudo apt-get update
 sudo apt-get install apache2 -y
 sudo systemctl enable apache2
 sudo systemctl start apache2
+sudo apt-get install mysql-client mysql-server -y
+sudo apt-get install php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-cli php7.0-cgi php7.0-gd -y
+sudo wget https://wordpress.org/latest.zip
+sudo apt-get install unzip -y
+sudo unzip latest.zip
 sudo rsync -av wordpress/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
+sudo cp /wp-config.php /var/www/html/wp-config.php
